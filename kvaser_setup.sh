246c0fbb69bd848fc401e77df19dd31a6a66fcfb
eldarial <darialdo92@hotmail.com>
2017-02-06 08:28:35 -0500

    added code

--- kvaser_setup.sh
+++ kvaser_setup.sh
@@ -1,7 +1,6 @@
- sudo modprobe can
- sudo modprobe can-raw
- sudo modprobe can-dev
- sudo modprobe kvaser_usb
- sudo ip link set can0 type can bitrate 500000
- sudo ifconfig can0 up
- #candump can0
+sudo modprobe can
+sudo modprobe can-raw
+sudo modprobe can-dev
+sudo modprobe kvaser_usb
+sudo ip link set can0 type can bitrate 500000
+sudo ifconfig can0 up
